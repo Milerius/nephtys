2fd492bff1ca65400e728a50a4f1133364f226d6
milerius <roman.sztergbaum@epitech.eu>
2019-04-10 18:33:40 +0200

    update linux deployment

--- scripts/deploy_linux.sh
+++ scripts/deploy_linux.sh
@@ -3,31 +3,25 @@
 function init() {
     root_dir=$PWD
     mkdir deploy_linux
-    rm -rf appimage_build
-    mkdir appimage_build
-    cd appimage_build
-    conan install ../
-    cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release ../
-    wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
-    chmod +x linuxdeploy-x86_64.AppImage
-    export PATH=$PATH:$PWD
+    rm -rf appimage_build && mkdir appimage_build && cd appimage_build
+    cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DAUTO_BUILD_APPIMAGE=ON ../
 }
 
 function deploy_client() {
     cd client
     make nephtys_client -j2
-    cp ./bin/*.AppImage ${root_dir}/deploy_linux/nephtys_client.AppImage
     cd ../
+    cp ./bin/nephtys_client-*.AppImage ${root_dir}/deploy_linux/nephtys_client.AppImage
 }
 
 function deploy_server() {
     cd server
     make nephtys_server -j2
-    make install DESTDIR=AppDir
-    linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage
-    cp ./*.AppImage ${root_dir}/deploy_linux/nephtys_server.AppImage
+    cd ../
+    cp ./bin/nephtys_server-*.AppImage ${root_dir}/deploy_linux/nephtys_server.AppImage
     cd ../
 }
+
 function clean() {
     rm -rf ${root_dir}/appimage_build
 }
