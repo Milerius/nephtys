4cae513419aad9f45467a877dddaf45c09a8e3ef
milerius <roman.sztergbaum@epitech.eu>
2019-04-22 12:25:32 +0200

    feat(resources): add windows executable path resolver

---
+++ common/common/include/nephtys/resources/details/windows/real_path.hpp
@@ -0,0 +1,14 @@
+//
+// Created by milerius on 06/04/19.
+//
+
+#pragma once
+
+#include <filesystem>
+
+namespace nephtys::client::details
+{
+    std::filesystem::path binary_real_path() noexcept;
+
+    std::filesystem::path assets_real_path() noexcept;
+}
---
+++ common/common/src/resources/details/windows/real_path.cpp
@@ -0,0 +1,29 @@
+//
+// Created by milerius on 06/04/19.
+//
+
+#include <WinBase.h> // GetLastError
+#include <libloaderapi.h> //! GetModuleHandleW, GetModuleFileNameW
+#include <string>
+#include <loguru.hpp>
+#include <nephtys/utils/pretty_function.hpp>
+#include <nephtys/resources/details/windows/real_path.hpp>
+
+namespace nephtys::client::details
+{
+    std::filesystem::path binary_real_path() noexcept
+    {
+      VLOG_SCOPE_F(loguru::Verbosity_INFO, pretty_function);
+      HMODULE hModule = GetModuleHandleW(NULL);
+      DCHECK_F(hModule != nullptr, "unable to get ModuleHandleW: %s", GetLastError());
+      WCHAR path[MAX_PATH];
+      auto result = GetModuleFileNameW(hModule, path, MAX_PATH);
+      DCHECK_F(result != 0, "unable to get executable path: %s", GetLastError());
+      return std::filesystem::path(path);
+    }
+
+    std::filesystem::path assets_real_path() noexcept
+    {
+      return binary_real_path();
+    }
+}
