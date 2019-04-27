c885708ef06971032128a9753624d8658e9d3b0b
Sztergbaum Roman <roman.sztergbaum@epitech.eu>
2019-04-27 08:13:19 +0200

    feat(world): enable timestep tick (#72)
    * feat(world): enable timestep tick
    
    * fix(typo)

--- client/include/nephtys/client/world/world.hpp
+++ client/include/nephtys/client/world/world.hpp
@@ -22,31 +22,37 @@ namespace nephtys::client
     public:
         world() noexcept
         {
-            VLOG_SCOPE_F(loguru::Verbosity_INFO, pretty_function);
-            DVLOG_F(loguru::Verbosity_INFO, "register quit_app event");
-            dispatcher_.sink<event::quit_app>().connect<&world::receive>(this);
+          VLOG_SCOPE_F(loguru::Verbosity_INFO, pretty_function);
+          DVLOG_F(loguru::Verbosity_INFO, "register quit_app event");
+          dispatcher_.sink<event::quit_app>().connect<&world::receive>(this);
         }
 
         void receive(const event::quit_app &evt) noexcept
         {
-            VLOG_SCOPE_F(loguru::Verbosity_INFO, pretty_function);
-            is_running_ = false;
-            exit_code_ = evt.exit_code;
+          VLOG_SCOPE_F(loguru::Verbosity_INFO, pretty_function);
+          is_running_ = false;
+          exit_code_ = evt.exit_code;
         }
 
         // LCOV_EXCL_START
         inline void run_once() noexcept
         {
-            input_system_.update();
+          timestep_.start_frame();
+          input_system_.update();
+          while (timestep_.is_update_required()) {
+            graphical_system_.update();
+            timestep_.perform_update();
+          }
         }
 
         inline st::exit_code run() noexcept
         {
-            is_running_ = true;
-            while (is_running_) {
-                run_once();
-            }
-            return exit_code_;
+          is_running_ = true;
+          timestep_.start();
+          while (is_running_) {
+            run_once();
+          }
+          return exit_code_;
         }
         // LCOV_EXCL_STOP
 
